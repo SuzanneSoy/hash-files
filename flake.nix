78c235875c7b7ce9f1b13a0789f7390a39020d83
Suzanne Soy <git@suzanne.soy>
2021-02-21 20:21:25 +0000

    install sha256sum with the option --zero using Nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "";
+  inputs = {
+    nixpkgs-git.url = https://github.com/NixOS/nixpkgs/archive/60330b60655b3fa3a18624bdd7069855bb37af99.tar.gz; # was: 62ef779f2a5050549772722665bedf52f01268d2
+  };
+  outputs = { nixpkgs-git, ... }@inputs : {
+   defaultPackage.x86_64-linux =
+     let system = "x86_64-linux"; in
+     let nixpkgs = import nixpkgs-git { config = { allowUnfree = true;}; system = system; }; in
+
+     let mypackages = with nixpkgs; [ coreutils ]; in
+
+     with import nixpkgs-git { system = "x86_64-linux"; };
+     symlinkJoin {
+       name = "mypackages";
+       paths = lib.lists.unique mypackages;
+       postBuild = ''echo "links created"'';
+     };
+  };
+}
